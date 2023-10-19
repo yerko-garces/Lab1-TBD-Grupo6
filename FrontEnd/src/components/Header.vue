f620c53006d23aba7347c1839866a50280df2cf0
Gabriel Ojeda Romero <gabriel.ojeda@usach.cl>
2023-10-19 16:40:00 -0300

    [Gabriel Ojeda] - Headers e Imagenes
    el menu lateral y superior, se dejo en un elemento reutilizable, headers, para ser utilizado en las distintas paginas.

---
+++ FrontEnd/src/components/Header.vue
@@ -0,0 +1,62 @@
+<template>
+    <v-app-bar>
+      <v-toolbar>
+          <img
+            alt="Vue logo"
+            class="logo"
+            src="@/assets/logo_sai_usach_4.png"
+            width="70"
+            margin-left="5"
+            height="70"
+            style="margin-left: 5px;"
+          />
+          <div class="text-subtitle-1 text-medium-emphasis">Grupo 6</div>
+        <v-spacer></v-spacer>
+        <v-btn variant="plain" to="/">
+          Cerrar Sesión
+          <v-icon class="icon"
+            ><img
+              alt="Cerrar sesión"
+              src="@/assets/cerrar-sesion.png"
+              width="25"
+              height="16"
+              style="margin-left: 8px;"
+          /></v-icon>
+        </v-btn>
+      </v-toolbar>
+    </v-app-bar>
+    <v-navigation-drawer app>
+    <v-list>
+      <v-list-item to="/..." class="borde">
+        <div class="center-text">Mi Perfil</div>
+      </v-list-item>
+      <v-list-item to="/..." class="borde">
+        <div class="center-text">Catástrofes</div>
+      </v-list-item>
+      <v-list-item to="/..." class="borde">
+        <div class="center-text">Tareas</div>
+      </v-list-item>
+    </v-list>
+    </v-navigation-drawer>
+  </template>
+<script>
+  export default {
+    name: "Header",
+  };
+</script>
+  
+<style scoped>
+.borde {
+  border: 1px solid #999999;
+  border-radius: 50px;
+  padding: 0px;
+  margin: 10px 10px;
+  font-weight: bold;
+}
+.center-text {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+</style>
--- FrontEnd/src/views/AboutView.vue
+++ FrontEnd/src/views/AboutView.vue
@@ -1,46 +1,41 @@
 <template>
   <v-layout class="rounded rounded-md">
-    <v-app-bar title="BIENVENIDO AL SISTEMA DE CATASTROFE"></v-app-bar>
-    <v-navigation-drawer app>
-    <v-list>
-      <v-list-item to="/..." class="borde">
-        <div class="center-text">Catastrofe</div>
-      </v-list-item>
-      <v-list-item to="/..." class="borde">
-        <div class="center-text">Historial</div>
-      </v-list-item>
-    </v-list>
-  </v-navigation-drawer>
-<v-main>
-  <div class="centered-layout">
     <Header />
-    <div class="centered-message">
-      <h1 class="titulo">Este es un servicio que permite gestionar de manera eficiente y efectiva las distintas catástrofes</h1>
-      <div class="content">
-        <img
-          alt="Vue logo"
-          class="logo"
-          src="@/assets/logo_sai_usach_4.png"
-          width="300"
-          height="300"
-        />
-        <!--<p class="titulo">Este es un servicio que permite gestionar de manera eficiente y efectiva las distintas catástrofes</p>-->
-        <br>
-        <ul>
-          <!-- <p class="descripcion">Este es un servicio que permite gestionar de manera eficiente y efectiva las distintas catástrofes.</p>-->
-        </ul>
-        <br>
-        <v-btn class="button" color="black" variant="outlined" to="/.">Botón 1</v-btn>
-        <v-btn class="button" color="black" variant="outlined" to="/.">Botón 2</v-btn>
+  <v-main>
+    <div class="centered-layout">
+      <div class="centered-message">
+        <h1 class="titulo">Este es un servicio que permite gestionar de manera eficiente y efectiva las distintas catástrofes</h1>
+        <div class="content">
+          <img
+            alt="Vue logo"
+            class="logo"
+            src="@/assets/logo_sai_usach_4.png"
+            width="300"
+            height="300"
+          />
+          <!--<p class="titulo">Este es un servicio que permite gestionar de manera eficiente y efectiva las distintas catástrofes</p>-->
+          <br>
+          <ul>
+            <!-- <p class="descripcion">Este es un servicio que permite gestionar de manera eficiente y efectiva las distintas catástrofes.</p>-->
+          </ul>
+          <br>
+          <v-btn class="button" color="black" variant="outlined" to="/.">Botón 1</v-btn>
+          <v-btn class="button" color="black" variant="outlined" to="/.">Botón 2</v-btn>
+        </div>
       </div>
     </div>
-  </div>
-</v-main>
+  </v-main>
   </v-layout>
 </template>
 
 <script>
-
+import axios from 'axios';
+import Header from "../components/Header.vue";
+export default {
+  components: {
+    Header,
+  }
+}
 </script>
 
 <style scoped>
@@ -55,19 +50,6 @@
 .centered-message {
   text-align: center;
   padding: 10px;
-}
-.borde {
-  border: 1px solid #999999;
-  border-radius: 50px;
-  padding: 0px;
-  margin: 10px 10px;
-  font-weight: bold;
-}
-.center-text {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
 }
 
 .titulo {
