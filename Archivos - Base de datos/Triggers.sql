eb7ad4f67a921f103e0df299b3e2184f9fb60373
Nachowo <ignacio.arias.t@usach.cl>
2023-10-19 17:35:01 -0300

    [Ignacio Arias] - implementado trigger para tabla de voluntarios

--- Archivos - Base de datos/Triggers.sql
+++ Archivos - Base de datos/Triggers.sql
@@ -2,21 +2,29 @@ CREATE TABLE logs (
     id SERIAL PRIMARY KEY,
     usuario_id BIGINT,
     descripcion TEXT,
-    timestamp TIMESTAMP
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
 );
 
-CREATE FUNCTION log_insert_voluntario()
+CREATE OR REPLACE FUNCTION log_voluntarios()
 RETURNS TRIGGER AS 
 $$
 BEGIN
-    INSERT INTO logs(usuario, descripcion, timestamp)
-    VALUES (NEW.id, 'insert en tabla voluntario', NOW());
+    IF TG_OP = 'INSERT' THEN
+        INSERT INTO logs (usuario_id, descripcion) VALUES (1,'Nueva inserción realizada');
+    ELSIF TG_OP = 'UPDATE' THEN
+        INSERT INTO logs (usuario_id, descripcion) VALUES (1,'Actualización realizada');
+    ELSIF TG_OP = 'DELETE' THEN
+        INSERT INTO logs (usuario_id, descripcion) VALUES (1,'Eliminación realizada');
+    END IF;
+    
     RETURN NEW;
 END;
 $$ 
 LANGUAGE plpgsql;
 
-CREATE TRIGGER insertVoluntario
-BEFORE INSERT ON Voluntario
-FOR EACH ROW EXECUTE FUNCTION log_insert_voluntario();
+CREATE TRIGGER trigger_consulta_voluntarios
+AFTER INSERT OR DELETE OR UODATE ON voluntario
+FOR EACH ROW
+EXECUTE FUNCTION log_voluntarios();
+
 
