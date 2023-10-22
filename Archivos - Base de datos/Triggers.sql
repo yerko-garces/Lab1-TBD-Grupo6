1c8e999f4a428b6d2ed9cc073abf66fa7a93ba1e
Nachowo <ignacio.arias.t@usach.cl>
2023-10-22 13:48:11 -0300

    [Ignacio Arias] - creado trigger para instituciones, emergencias y habilidades

--- Archivos - Base de datos/Triggers.sql
+++ Archivos - Base de datos/Triggers.sql
@@ -1,3 +1,5 @@
+-- tabla de logs
+
 CREATE TABLE IF NOT EXISTS logs (
     id SERIAL PRIMARY KEY,
     idUsuario BIGINT,
@@ -5,24 +7,19 @@ CREATE TABLE IF NOT EXISTS logs (
     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
 );
 
+
+--funcion para tabla voluntarios
+
 CREATE OR REPLACE FUNCTION log_voluntarios()
 RETURNS TRIGGER AS 
 $$
 BEGIN
     IF TG_OP = 'INSERT' THEN
-<<<<<<< Updated upstream
-        INSERT INTO logs (idUsuario, descripcion) VALUES (1,'Nueva inserción realizada');
-=======
         INSERT INTO logs (idUsuario, descripcion) VALUES (NEW.idvoluntario,'Nueva inserción realizada');
->>>>>>> Stashed changes
     ELSIF TG_OP = 'UPDATE' THEN
         INSERT INTO logs (idUsuario, descripcion) VALUES (NEW.idvoluntario,'Actualización realizada');
     ELSIF TG_OP = 'DELETE' THEN
-<<<<<<< Updated upstream
-        INSERT INTO logs (idUsuario, descripcion) VALUES (1,'Eliminación realizada');
-=======
         INSERT INTO logs (idUsuario, descripcion) VALUES (OLD.idvoluntario,'Eliminación realizada');
->>>>>>> Stashed changes
     END IF;
     
     RETURN NEW;
@@ -30,10 +27,10 @@ END;
 $$ 
 LANGUAGE plpgsql;
 
--- Elimina el trigger si existe
+--trigger
+
 DROP TRIGGER IF EXISTS trigger_consulta_voluntarios ON voluntario;
 
--- Crea el nuevo trigger
 CREATE TRIGGER trigger_consulta_voluntarios
 AFTER INSERT OR DELETE OR UPDATE ON voluntario
 FOR EACH ROW
@@ -41,3 +38,83 @@ EXECUTE FUNCTION log_voluntarios();
 
 
 
+-- funcion para voluntarioHabilidad
+CREATE OR REPLACE FUNCTION log_voluntarioHabilidad()
+RETURNS TRIGGER AS 
+$$
+BEGIN
+    IF TG_OP = 'INSERT' THEN
+        INSERT INTO logs (idUsuario, descripcion) VALUES (NEW.idvoluntario,'El usuario insert una habilidad');
+    ELSIF TG_OP = 'UPDATE' THEN
+        INSERT INTO logs (idUsuario, descripcion) VALUES (NEW.idvoluntario,'El usuario actualizo una habilidad');
+    ELSIF TG_OP = 'DELETE' THEN
+        INSERT INTO logs (idUsuario, descripcion) VALUES (OLD.idvoluntario,'El usuario elimino una habilidad');
+    END IF;
+    
+    RETURN NEW;
+END;
+$$ 
+LANGUAGE plpgsql;
+
+--trigger
+
+DROP TRIGGER IF EXISTS trigger_voluntarioHabilidad ON voluntarioHabilidad;
+
+CREATE TRIGGER trigger_voluntarioHabilidad
+AFTER INSERT OR DELETE OR UPDATE ON voluntarioHabilidad
+FOR EACH ROW
+EXECUTE FUNCTION log_voluntarioHabilidad();
+
+-- funcion para Emergencias
+CREATE OR REPLACE FUNCTION log_emergencia()
+RETURNS TRIGGER AS 
+$$
+BEGIN
+    IF TG_OP = 'INSERT' THEN
+        INSERT INTO logs (idUsuario, descripcion) VALUES (NEW.idinstitucion,'la institucion ha creado una emergencia');
+    ELSIF TG_OP = 'UPDATE' THEN
+        INSERT INTO logs (idUsuario, descripcion) VALUES (NEW.idinstitucion,'la institucion ha actualizado una emergencia');
+    ELSIF TG_OP = 'DELETE' THEN
+        INSERT INTO logs (idUsuario, descripcion) VALUES (OLD.idinstitucion,'la institucion ha eliminado una emergencia');
+    END IF;
+    
+    RETURN NEW;
+END;
+$$ 
+LANGUAGE plpgsql;
+
+--trigger
+
+DROP TRIGGER IF EXISTS trigger_emergencia ON emergencia;
+
+CREATE TRIGGER trigger_emergencia
+AFTER INSERT OR DELETE OR UPDATE ON emergencia
+FOR EACH ROW
+EXECUTE FUNCTION log_emergencia();
+
+-- funcion para Instituciones
+CREATE OR REPLACE FUNCTION log_institucion()
+RETURNS TRIGGER AS 
+$$
+BEGIN
+    IF TG_OP = 'INSERT' THEN
+        INSERT INTO logs (idUsuario, descripcion) VALUES (NEW.idinstitucion,'Se ha creado la institucion');
+    ELSIF TG_OP = 'UPDATE' THEN
+        INSERT INTO logs (idUsuario, descripcion) VALUES (NEW.idinstitucion,'Se ha actualizado la institucion');
+    ELSIF TG_OP = 'DELETE' THEN
+        INSERT INTO logs (idUsuario, descripcion) VALUES (OLD.idinstitucion,'Se ha eliminado la institucion');
+    END IF;
+    
+    RETURN NEW;
+END;
+$$ 
+LANGUAGE plpgsql;
+
+--trigger
+
+DROP TRIGGER IF EXISTS trigger_institucion ON institucion;
+
+CREATE TRIGGER trigger_institucion
+AFTER INSERT OR DELETE OR UPDATE ON institucion
+FOR EACH ROW
+EXECUTE FUNCTION log_institucion();
